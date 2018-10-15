8490808701438e9374dd123d1f1618040bc2fadd
Kacper <kap3r3k@gmail.com>
2018-10-15 19:06:43 +0200

    Dodge roll tweaked, Interaction and attack methods added, Unity updated
    Sheer beginnings of the functions added. Interaction picks up weapons

---
+++ Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour {
+
+    public float weaponDMG = 100f;
+    public float weaponCoolDown = 5.0f;
+    public float weaponComboTimer = 0.5f;
+}
